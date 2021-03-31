e5c487a110abf11dc82a9dd0c5217b39472c6745
Your Name <you@example.com>
2021-03-31 17:21:39 +0430

    create tests method TODO level

---
+++ Test/UsersRepoTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/php/Controller/UsersController.php';
+
+class UsersRepositoryTest extends TestCase
+{
+
+    # create section
+    public function testAddNewUser()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+
+
+    # read section
+
+    public function testReadAllUsers()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+
+    public function testGetUserById()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+
+    public function testGetUserByUsername()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+
+    public function testGetUserByEmail()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+
+
+    # update section
+
+    public function testUpdateUser()
+    {
+        #TODO()
+        $this->assertTrue(true);
+    }
+
+
+    # delete section
+
+    public function testDeleteUserById()
+    {
+        # TODO()
+        $this->assertTrue(true);
+    }
+
+    public function testDeleteUserByUsername()
+    {
+        #TODO()
+        $this->assertTrue(true);
+    }
+
+    public function testDeleteUserByEmail()
+    {
+        #TODO
+        $this->assertTrue(true);
+    }
+}
