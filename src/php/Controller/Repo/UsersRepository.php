4a67ae3d5c58036e3c6ad4e232012a1007304526
Your Name <you@example.com>
2021-03-31 15:00:29 +0430

    create all methods in UserRepository and declare TODO

--- src/php/Controller/Repo/UsersRepository.php
+++ src/php/Controller/Repo/UsersRepository.php
@@ -1,5 +1,87 @@
 <?php
 
+use phpDocumentor\Reflection\Types\Boolean;
+
+require_once __DIR__ . '/../UsersController.php';
+
+
 class UsersRepository
 {
+    public function __construct()
+    {
+        # TODO()
+    }
+
+    # create section
+    public function addNewUser(UsersModel $user): bool
+    {
+        # TODO()
+
+        return true;
+    }
+
+
+    # read section
+
+    public function readAllUsers(): ?mysqli_result
+    {
+        # TODO()
+
+        return null;
+    }
+
+    public function getUserById($id): ?mysqli_result
+    {
+        # TODO()
+
+        return null;
+    }
+
+    public function getUserByUsername($username): ?mysqli_result
+    {
+        # TODO()
+
+        return null;
+    }
+
+    public function getUserByEmail($email): ?mysqli_result
+    {
+        # TODO()
+
+        return null;
+    }
+
+
+    # update section
+
+    public function updateUser(UsersModel $user): ?bool
+    {
+        #TODO()
+
+        return null;
+    }
+
+
+    # delete section
+
+    public function deleteUserById($id): ?bool
+    {
+        # TODO()
+
+        return null;
+    }
+
+    public function deleteByUsername($username): ?bool
+    {
+        #TODO()
+
+        return null;
+    }
+
+    public function deleteByEmail($email): ?bool
+    {
+        #TODO
+
+        return null;
+    }
 }
