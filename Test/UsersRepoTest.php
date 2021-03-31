a85321f9c2e64ba91c7996f14d4ff2d5ced96f9a
Your Name <you@example.com>
2021-03-31 17:32:01 +0430

    add $repo = new UsersRepository(); to all test methods.

--- Test/UsersRepoTest.php
+++ Test/UsersRepoTest.php
@@ -4,12 +4,14 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../src/php/Controller/UsersController.php';
 
+
 class UsersRepositoryTest extends TestCase
 {
-
     # create section
     public function testAddNewUser()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
@@ -19,36 +21,48 @@ class UsersRepositoryTest extends TestCase
 
     public function testReadAllUsers()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
 
     public function testGetUserById()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
 
     public function testGetUserByUsername()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
 
     public function testGetUserByEmail()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
 
     public function testGetFirstUser()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
 
     public function testGetLastUser()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
@@ -57,6 +71,8 @@ class UsersRepositoryTest extends TestCase
 
     public function testUpdateUser()
     {
+        $repo = new UsersRepository();
+
         #TODO()
         $this->assertTrue(true);
     }
@@ -66,18 +82,24 @@ class UsersRepositoryTest extends TestCase
 
     public function testDeleteUserById()
     {
+        $repo = new UsersRepository();
+
         # TODO()
         $this->assertTrue(true);
     }
 
     public function testDeleteUserByUsername()
     {
+        $repo = new UsersRepository();
+
         #TODO()
         $this->assertTrue(true);
     }
 
     public function testDeleteUserByEmail()
     {
+        $repo = new UsersRepository();
+
         #TODO
         $this->assertTrue(true);
     }
