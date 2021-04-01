1e734dc4c8bf93bb1c8c5e05707b0723c63c88fd
Your Name <you@example.com>
2021-04-02 00:20:53 +0430

    add bootstrap and jquery.

--- public/index.php
+++ public/index.php
@@ -6,11 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="src/View/css/public.css">
 </head>
 
 <body>
 
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
     <script src="src/View/js/index.js"></script>
 </body>
 
