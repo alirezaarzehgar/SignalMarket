acec1b9c432f5ae5b0243bd99963b0de7ff1a2af
Your Name <you@example.com>
2021-04-02 23:16:15 +0430

    create admin edit delete ui

--- public/admin/add-and-edit-admins.php
+++ public/admin/add-and-edit-admins.php
@@ -38,9 +38,11 @@ if (
     header("location: /public/admin");
 }
 
-// calculate admin permission
-$permission = $repo->getAdminByUsername($_COOKIE['admin'])['permission'];
-$_SESSION['permission'] = $permission;
+if ($_SESSION['permission'] == 0) {
+    header("location: /public/");
+}
+
+$allAdmins = $repo->readAllAdmins();
 
 ?>
 
@@ -51,10 +53,18 @@ $_SESSION['permission'] = $permission;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Admin</title>
+    <title>
+        <?php if ($_SESSION['permission'] == Permission::$read) : ?>
+            See Admins
+        <?php elseif ($_SESSION['permission'] == Permission::$write) : ?>
+            Admin CRUD
+        <?php endif ?>
+    </title>
+
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="/node_modules/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/public/View/css/admin.css">
+    <link rel="stylesheet" href="/public/View/css/add-and-edit-admins.css">
 
 </head>
 
@@ -69,13 +79,8 @@ $_SESSION['permission'] = $permission;
             <div class="dropdown">
                 <button class="btn dropdown-toggle" type="button" data-toggle="dropdown"><?= $_COOKIE['admin'] ?></button>
                 <ul class="dropdown-menu">
+                    <li><a href="/public/">Home</a></li>
                     <li><a href="/public/admin/edit-password.php">Edit Password</a></li>
-                    <?php if ($permission == Permission::$read) : ?>
-                        <li><a href="/public/admin/add-and-edit-admins.php">see another admins</a></li>
-                    <?php elseif ($permission == Permission::$write) : ?>
-                        <li><a href="/public/admin/add-and-edit-admins.php">Add & Edit another admins</a></li>
-                    <?php endif ?>
-
                     <li><a href="/public/admin/index.php?request=logout">Logout</a></li>
                 </ul>
             </div>
@@ -83,7 +88,35 @@ $_SESSION['permission'] = $permission;
     </header>
 
     <main>
+        <div class="container-fluid p-5 d-flex flex-wrap">
+            <?php foreach ($allAdmins as $value) : ?>
+                <?php $currentPerm = $repo->getAdminByUsername($value['username'])['permission'] ?>
+                <?php $accessIntro = "" ?>
 
+                <div class="container border shadow m-5 text-center w-auto overflow-hidden" id="admin-cart">
+                    <?php if ($currentPerm == Permission::$read) : ?>
+                        <img class="m-2 mb-4" src="/public/View/img/read-access.png" alt="read access">
+                        <?php $accessIntro = "Just can see another admins." ?>
+                    <?php elseif ($currentPerm == Permission::$write) : ?>
+                        <img class="m-2 mb-4" src="/public/View/img/write-access.png" alt="write access">
+                        <?php $accessIntro = "Super admin.<br>
+                         He can: <br>
+                         <ul>
+                            <li>Create new admin</li>
+                            <li>Edit exist admins</li>
+                            <li>Delete exist admins</li>
+                         </ul>" ?>
+                    <?php else : ?>
+                        <img class="m-2 mb-4" src="/public/View/img/limited.png" alt="limited">
+
+                        <?php $accessIntro = "he has no access" ?>
+                    <?php endif ?>
+
+                    <h1><?= $value['username'] ?></h1>
+                    <h6 class="text-left p-3"><?= $accessIntro ?></h6>
+                </div>
+            <?php endforeach ?>
+        </div>
     </main>
 
     <footer>
@@ -93,7 +126,7 @@ $_SESSION['permission'] = $permission;
     <script src="/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="/node_modules/popper.js/dist/popper.min.js"></script>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="/public/View/js/admin.js"></script>
+    <script src="/public/View/js/add-and-edit-admins.js"></script>
 </body>
 
 </html>
