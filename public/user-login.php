40fc0b468d5f8da09ca62772da8a566cc56969dd
Your Name <you@example.com>
2021-04-02 06:19:11 +0430

    add same template to all php files.

---
+++ public/user-login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Admin</title>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="View/css/public.css">
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main class="container">
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="View/js/index.js"></script>
+</body>
+
+</html>
