a4b28fcf973d5b39c8b7e2b133c9553594d9e3ff
Your Name <you@example.com>
2021-04-03 08:02:15 +0430

    add update api and comment

--- api/admin_api.php
+++ api/admin_api.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../src/php/Controller/Repo/AdminsRepository.php';
 
 $repo = new AdminsRepository();
 
+// search to usernames
 if ($_GET['req'] == 'search in usernames' and isset($_GET['username'])) {
     $user = $repo->getAdminByUsername($_GET['username']);
 
@@ -14,6 +15,7 @@ if ($_GET['req'] == 'search in usernames' and isset($_GET['username'])) {
         echo '{"status": 404}';
 }
 
+// search to passwords
 if (
     $_GET['req'] == 'search in passwords' and
     isset($_GET['username']) and
@@ -23,6 +25,36 @@ if (
 
     if (!empty($user) and $user['password'] == Hashing::encrypt($_GET['password']))
         echo '{"status": 200}';
+    else
+        echo '{"status": 404}';
+}
+
+
+// update admin
+if (
+    $_GET['req'] == 'update' and
+    isset($_GET['username'])
+) {
+    $username = $_GET['username'];
+    $id = $repo->getAdminByUsername($username)['id'];
+    $password = null;
+    if (!empty($_GET['password']))
+        $password = $_GET['password'];
+
+    $admin = new AdminsModel(
+        username: $username,
+        password: $password,
+        permission: $_GET['permission']
+    );
+
+    $reult = $repo->updateAdmin(
+        admin: $admin,
+        id: $id
+    );
+
+    if ($reult)
+        echo '{"status": 200,
+            "permission": ' . $_GET['permission'] . '}';
     else
         echo '{"status": 404}';
 }
