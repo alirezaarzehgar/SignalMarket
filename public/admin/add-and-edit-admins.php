2212c3c27d2fd2fdeceb4d7c0be84fa966991710
Your Name <you@example.com>
2021-04-03 08:02:58 +0430

    patch php code to edit, see users

--- public/admin/add-and-edit-admins.php
+++ public/admin/add-and-edit-admins.php
@@ -61,10 +61,17 @@ $allAdmins = $repo->readAllAdmins();
         <?php endif ?>
     </title>
 
+    <!-- css files -->
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="/node_modules/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/public/View/css/admin.css">
     <link rel="stylesheet" href="/public/View/css/add-and-edit-admins.css">
+
+    <!-- in this page i use js function between php code ... we sould include js libs on top of page. -->
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="/node_modules/popper.js/dist/popper.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="/public/View/js/add-and-edit-admins.js"></script>
 
 </head>
 
@@ -90,15 +97,19 @@ $allAdmins = $repo->readAllAdmins();
     <main>
         <div class="container-fluid p-5 d-flex flex-wrap">
             <?php foreach ($allAdmins as $value) : ?>
+
+                <?php if ($value['username'] == $_SESSION['admin']) continue; ?>
+
                 <?php $currentPerm = $repo->getAdminByUsername($value['username'])['permission'] ?>
                 <?php $accessIntro = "" ?>
+                <?php $topIcon = "" ?>
 
-                <div class="container border shadow m-5 text-center w-auto overflow-hidden" id="admin-cart">
+                <div class="admin-cart container border shadow m-5 text-center w-auto overflow-hidden d-flex flex-column align-items-center <?php if ($_SESSION['permission'] == Permission::$write) echo "write-access"; ?>" id="admin-cart-<?= $value['username'] ?>">
                     <?php if ($currentPerm == Permission::$read) : ?>
-                        <img class="m-2 mb-4" src="/public/View/img/read-access.png" alt="read access">
+                        <?php $topIcon = "/public/View/img/read-access.png" ?>
                         <?php $accessIntro = "Just can see another admins." ?>
                     <?php elseif ($currentPerm == Permission::$write) : ?>
-                        <img class="m-2 mb-4" src="/public/View/img/write-access.png" alt="write access">
+                        <?php $topIcon = "/public/View/img/write-access.png" ?>
                         <?php $accessIntro = "Super admin.<br>
                          He can: <br>
                          <ul>
@@ -107,13 +118,41 @@ $allAdmins = $repo->readAllAdmins();
                             <li>Delete exist admins</li>
                          </ul>" ?>
                     <?php else : ?>
-                        <img class="m-2 mb-4" src="/public/View/img/limited.png" alt="limited">
+                        <?php $topIcon = "/public/View/img/limited.png" ?>
 
                         <?php $accessIntro = "he has no access" ?>
                     <?php endif ?>
 
-                    <h1><?= $value['username'] ?></h1>
-                    <h6 class="text-left p-3"><?= $accessIntro ?></h6>
+                    <img id="access-image-<?= $value['username'] ?>" class="m-2 mb-4" src="<?= $topIcon ?>" alt="write access">
+
+                    <h1 class="flex-grow-1"><?= $value['username'] ?></h1>
+                    <h6 class="text-left p-3" id="intro-<?= $value['username'] ?>"><?= $accessIntro ?></h6>
+
+                    <div class="<?php if ($_SESSION['permission'] == Permission::$write) echo "overally"; ?>">
+                        <div class="container p-3 h-100">
+                            <div class="d-flex flex-column justify-content-around h-100">
+                                <h2> Edit Admin </h2>
+
+                                <h4 class="text-center text-white border p-2"><?= $value['username'] ?></h4>
+                                <div>
+                                    <input class="m-1" type="password" name="password" id="password-<?= $value['username'] ?>" placeholder="Enter new password" autocomplete="off">
+                                    <p class="text-white ml-3" id="password-finished-<?= $value['username'] ?>"></p>
+
+                                    <select class="permission" id="permission-<?= $value['username'] ?>">
+                                        <option value="0" <?php if ($value['permission'] == 0) echo "selected"; ?>>0</option>
+                                        <option value="2" <?php if ($value['permission'] == 2) echo "selected"; ?>>2</option>
+                                        <option value="6" <?php if ($value['permission'] == 6) echo "selected"; ?>>6</option>
+                                    </select>
+                                </div>
+
+                                <button class="m-1 btn btn-secondary text-center" id="submit-<?= $value['username'] ?>">Update</button>
+
+                                <script>
+                                    handleCarts('<?= $value['username'] ?>')
+                                </script>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             <?php endforeach ?>
         </div>
@@ -122,11 +161,6 @@ $allAdmins = $repo->readAllAdmins();
     <footer>
 
     </footer>
-
-    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="/node_modules/popper.js/dist/popper.min.js"></script>
-    <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="/public/View/js/add-and-edit-admins.js"></script>
 </body>
 
 </html>
