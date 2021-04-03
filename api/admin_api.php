af4567a755e69ea6ded11cfb65b8c4b18d84191f
Your Name <you@example.com>
2021-04-03 12:22:46 +0430

    remove auto accetp password for login

--- api/admin_api.php
+++ api/admin_api.php
@@ -1,5 +1,20 @@
 <?php
 
+function auth()
+{
+    if (
+        !isset($_SESSION['admin']) or
+        !isset($_SESSION['permission']) or
+        $_SESSION['permission'] != 6
+    ) {
+        echo "خوب اومدی جلو XD";
+        exit();
+    }
+}
+
+session_start();
+
+
 require_once __DIR__ . '/../src/php/Controller/Repo/AdminsRepository.php';
 
 
@@ -15,26 +30,13 @@ if ($_GET['req'] == 'search in usernames' and isset($_GET['username'])) {
         echo '{"status": 404}';
 }
 
-// search to passwords
-if (
-    $_GET['req'] == 'search in passwords' and
-    isset($_GET['username']) and
-    isset($_GET['password'])
-) {
-    $user = $repo->getAdminByUsername($_GET['username']);
-
-    if (!empty($user) and $user['password'] == Hashing::encrypt($_GET['password']))
-        echo '{"status": 200}';
-    else
-        echo '{"status": 404}';
-}
-
-
 // update admin
 if (
     $_GET['req'] == 'update' and
     isset($_GET['username'])
 ) {
+    auth();
+
     $username = $_GET['username'];
     $id = $repo->getAdminByUsername($username)['id'];
     $password = null;
@@ -57,4 +59,26 @@ if (
             "permission": ' . $_GET['permission'] . '}';
     else
         echo '{"status": 404}';
+}
+
+// delete admin
+if (
+    $_GET['req'] == 'delete' and
+    isset($_GET['username'])
+) {
+    auth();
+
+    $username = $_GET['username'];
+
+    if (empty($repo->getAdminByUsername($username))) {
+        echo '{"status": 404}';
+        exit();
+    }
+
+    $result = $repo->deleteAdminByUsername($username);
+    if ($result) {
+        echo '{"status": 200}';
+    } else {
+        echo '{"status": 404}';
+    }
 }
--- public/View/js/admin-login.js
+++ public/View/js/admin-login.js
@@ -12,35 +12,12 @@ $("#username").on("keyup", function () {
 
       if (status == 200) {
         $("#username-finished").text('user "' + user + '" exists.');
+        $("#submit").prop("disabled", false);
         $("#password").prop("disabled", false);
       } else {
         $("#username-finished").text("");
-        $("#password").prop("disabled", true);
-      }
-    }
-  );
-});
-
-$("#password").on("keyup", function () {
-  $.get(
-    "/api/admin_api.php",
-    {
-      req: "search in passwords",
-      username: $("#username").val(),
-      password: $("#password").val(),
-    },
-    function (data, textStatus, jqXHR) {
-      var status = JSON.parse(data).status;
-
-      var user = $("#username").val();
-      var pass = $("#password").val();
-
-      if (status == 200) {
-        $("#password-finished").text("correct password");
-        $("#submit").prop("disabled", false);
-      } else {
-        $("#password-finished").text("");
         $("#submit").prop("disabled", true);
+        $("#password").prop("disabled", true);
       }
     }
   );
