f1684d6645b4123229a7951988fe8ecb8a0f9b65
Your Name <you@example.com>
2021-04-05 14:24:15 +0430

    implement product_api.php.

--- api/product_api.php
+++ api/product_api.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "ji";
+require_once __DIR__ . '/../src/php/Common/auth.php';
+require_once __DIR__ . '/../src/php/Controller/Repo/ProductsRepository.php';
+
+
+$repo = new ProductsRepository();
+
+if (
+    $_POST['req'] == "delete" and
+    isset($_POST['id'])
+) {
+    auth();
+
+    $result = $repo->deleteProductById($_POST['id']);
+
+    if ($result) {
+        echo '{"status": 200}';
+    } else {
+        echo '{"status": 404}';
+    }
+}
