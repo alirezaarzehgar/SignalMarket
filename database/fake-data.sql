71bce16d6d9f564545e79587f8ac3f446eafdc33
Your Name <you@example.com>
2021-04-05 14:18:34 +0430

    s

--- database/fake-data.sql
+++ database/fake-data.sql
@@ -186,27 +186,3 @@ INSERT INTO admins
 );
 
 SELECT * FROM admins;
-
-
-INSERT INTO products (
-    admin_name,
-    subject,
-    photo_dir_path,
-    introduction_to_product,
-
-    choosen_by_customer,
-
-    choosen_by_admin,
-
-    success_payment
-) VALUES (
-    'ali',
-    'subject',
-    '/path',
-    'intro',
-    0,
-    0,
-    0
-);
-
-SELECT * FROM products;
